cef1e7a34662ca1270c2e90294dc9f169007fe3d
Michael Houmann <mhoumann87@gmail.com>
2021-06-13 10:04:27 +0200

    Setup server to use static pages and add index, about and help

--- src/app.js
+++ src/app.js
@@ -1,10 +1,32 @@
+// Core node libraries
+const path = require('path');
+// import dependencies
 const express = require('express');
 
-const app = express();
-const port = 3000;
+// Setup const to hold port
+const port = process.env.PORT || 3000;
+// Setup const to hold path to public directory
+const publicDir = path.join(__dirname, '../public');
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello from Express</h1>');
+// Initialize express in an const
+const app = express();
+
+// Set the project to serve static files
+app.use(express.static(path.join(publicDir)));
+
+// app.get('/help', (req, res) => {
+//   res.sendFile(publicDir, '/help.html');
+// });
+
+// app.get('/about', (req, res) => {
+//   res.sendFile(publicDir, 'about.html');
+// });
+
+app.get('/weather', (req, res) => {
+  res.send({
+    forecast: 'snowing',
+    temperature: -10.9,
+  });
 });
 
 app.listen(port, () => console.log(`App is running on port ${port}`));
