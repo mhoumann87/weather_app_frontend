a49b7a9ca2e9bb38a112cc618a4239fe6d7e6407
Michael Houmann <mhoumann87@gmail.com>
2021-06-13 21:53:20 +0200

    Change the name of templates dir, refactor app.js :recycle:

--- src/app.js
+++ src/app.js
@@ -3,19 +3,28 @@ const path = require('path');
 // import dependencies
 const express = require('express');
 
-// Setup const to hold port
-const port = process.env.PORT || 3005;
-// Setup const to hold path to public directory
-const publicDir = path.join(__dirname, '../public');
-
 // Initialize express in an const
 const app = express();
+
+// Setup const to hold port
+const port = process.env.PORT || 3005;
+
+// Define paths for express config
+// Setup const to hold path to public directory
+const publicDir = path.join(__dirname, '../public');
+// Set up path to template directory
+const viewDir = path.join(__dirname, '../templates');
 
 // Set the project to serve static files
 app.use(express.static(path.join(publicDir)));
 
-// Set view engine to handelbars
+// handlebar configuration
+// tell express to use handlebar
 app.set('view engine', 'hbs');
+// set the directory for the handlebar files
+app.set('views', viewDir);
+
+// Setup routes for the site
 
 app.get('/', (req, res) => {
   res.render('index', {
@@ -43,4 +52,5 @@ app.get('/weather', (req, res) => {
   });
 });
 
+// "Start" the server
 app.listen(port, () => console.log(`App is running on port ${port}`));
